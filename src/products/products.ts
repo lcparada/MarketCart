b7acd0220b280ebe3bd84a6c1db22c228b668287
lcparada <125614063+lcparada@users.noreply.github.com>
2023-04-16 16:54:48 -0300

    feat: list of products

---
+++ src/products/products.ts
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: "1",
+    name: "Coca cola",
+    unitPrice: 19.9,
+    price: 19.9,
+  },
+  {
+    id: "2",
+    name: "Chocolate",
+    unitPrice: 6.5,
+    price: 6.5,
+  },
+  {
+    id: "3",
+    name: "Cheese 500g",
+    unitPrice: 15,
+    price: 15,
+  },
+  {
+    id: "4",
+    name: "French Fries",
+    unitPrice: 23.9,
+    price: 23.9,
+  },
+  {
+    id: "5",
+    name: "Guarana",
+    unitPrice: 6,
+    price: 6,
+  },
+];
+
+export { products };
